c14f6359e2b2b8e8aa4bb3bf4138c584c4adeb8c
Michael Weiner <michael.weiner@posteo.de>
2026-04-19 15:12:48 +0200

    phone_fsm: improve handling of disconnected BT audio during active call

--- Core/Src/phone_fsm.c
+++ Core/Src/phone_fsm.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "main.h"
 #include "bt_hfp.h"
+#include "main.h"
 #include "ringer.h"
 #include "rotary.h"
 #include "tone.h"
@@ -16,38 +16,45 @@ enum phone_state_t
     PHONE_DIALTONE,
     PHONE_ACTIVE_CALL,
     PHONE_INCOMING_CALL,
+    PHONE_AUDIO_DISCONNECTED_FROM_CALL,
     PHONE_ERROR
 };
 
 static enum phone_state_t phone_state = PHONE_IDLE;
 static UART_HandleTypeDef *huart_at = NULL;
 
-void phone_fsm_init(UART_HandleTypeDef *huart)
-{
-    huart_at = huart;
-}
+void phone_fsm_init(UART_HandleTypeDef *huart) { huart_at = huart; }
 
 void phone_fsm_process(void)
 {
     GPIO_PinState pin_gu = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_3);
     bt_hfp_process();
-    enum hfpstat_t hfp_stat = bt_hfp_get_stat();
+    enum hfpstat_t hfp_state = bt_hfp_get_stat();
+    enum hfpaudio_t audio_state = bt_hfp_get_audio();
     enum phone_state_t old_phone_state = phone_state;
     switch (phone_state)
     {
     case PHONE_IDLE:
         if (pin_gu == GPIO_PIN_SET)
         {
-            if (hfp_stat == HFPSTAT_CONNECTED)
+            if (hfp_state == HFPSTAT_CONNECTED)
             {
                 start_dialtone(DIALTONE_DEFAULT);
                 phone_state = PHONE_DIALTONE;
             }
-            else if (hfp_stat == HFPSTAT_OUTGOING_CALL ||
-                     hfp_stat == HFPSTAT_ACTIVE_CALL)
+            else if (hfp_state == HFPSTAT_OUTGOING_CALL ||
+                     hfp_state == HFPSTAT_ACTIVE_CALL)
             {
-                HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_SET);
-                phone_state = PHONE_ACTIVE_CALL;
+                if (audio_state == HFPAUDIO_CONNECTED)
+                {
+                    HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin,
+                                      GPIO_PIN_SET);
+                    phone_state = PHONE_ACTIVE_CALL;
+                }
+                else
+                {
+                    phone_state = PHONE_AUDIO_DISCONNECTED_FROM_CALL;
+                }
             }
             else
             {
@@ -55,7 +62,7 @@ void phone_fsm_process(void)
                 phone_state = PHONE_ERROR;
             }
         }
-        else if (hfp_stat == HFPSTAT_INCOMING_CALL)
+        else if (hfp_state == HFPSTAT_INCOMING_CALL)
         {
             start_bell();
             phone_state = PHONE_INCOMING_CALL;
@@ -64,8 +71,8 @@ void phone_fsm_process(void)
         {
             if (bt_hfp_audio_changed())
             {
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13,
-                                  bt_hfp_get_audio() == HFPAUDIO_CONNECTED
+                HAL_GPIO_WritePin(HV_EN_GPIO_Port, HV_EN_Pin,
+                                  audio_state == HFPAUDIO_CONNECTED
                                       ? GPIO_PIN_SET
                                       : GPIO_PIN_RESET);
             }
@@ -96,7 +103,8 @@ void phone_fsm_process(void)
             {
                 HAL_UART_Transmit(huart_at, (const uint8_t *)command,
                                   strlen(command), 100);
-                HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin,
+                                  GPIO_PIN_SET);
                 phone_state = PHONE_ACTIVE_CALL;
             }
             else
@@ -104,11 +112,19 @@ void phone_fsm_process(void)
                 phone_state = PHONE_IDLE;
             }
         }
+        else if (((hfp_state == HFPSTAT_OUTGOING_CALL) ||
+                  (hfp_state == HFPSTAT_ACTIVE_CALL)) &&
+                 (audio_state == HFPAUDIO_CONNECTED))
+        {
+            stop_dialtone();
+            HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_SET);
+            phone_state = PHONE_ACTIVE_CALL;
+        }
         break;
 
     case PHONE_INCOMING_CALL:
-        if ((hfp_stat != HFPSTAT_INCOMING_CALL) &&
-            (bt_hfp_get_audio() != HFPAUDIO_CONNECTED))
+        if ((hfp_state != HFPSTAT_INCOMING_CALL) &&
+            (audio_state != HFPAUDIO_CONNECTED))
         {
             stop_bell();
             phone_state = PHONE_IDLE;
@@ -117,8 +133,8 @@ void phone_fsm_process(void)
         {
             uint8_t command[] = "AT+HFPANSW\r\n";
             stop_bell();
-            HAL_UART_Transmit(huart_at, command,
-                              strlen((const char *)command), 100);
+            HAL_UART_Transmit(huart_at, command, strlen((const char *)command),
+                              100);
             HAL_Delay(100);
             HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_SET);
             phone_state = PHONE_ACTIVE_CALL;
@@ -129,9 +145,27 @@ void phone_fsm_process(void)
         if (pin_gu == GPIO_PIN_RESET)
         {
             uint8_t command[] = "AT+HFPCHUP\r\n";
-            HAL_UART_Transmit(huart_at, command,
-                              strlen((const char *)command), 100);
+            HAL_UART_Transmit(huart_at, command, strlen((const char *)command),
+                              100);
             HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_RESET);
+            phone_state = PHONE_IDLE;
+        }
+        else if (audio_state == HFPAUDIO_DISCONNECTED)
+        {
+            HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_RESET);
+            phone_state = PHONE_AUDIO_DISCONNECTED_FROM_CALL;
+        }
+        break;
+
+    case PHONE_AUDIO_DISCONNECTED_FROM_CALL:
+        if ((audio_state == HFPAUDIO_CONNECTED) &&
+            (hfp_state == HFPSTAT_ACTIVE_CALL))
+        {
+            HAL_GPIO_WritePin(VOICE_EN_GPIO_Port, VOICE_EN_Pin, GPIO_PIN_SET);
+            phone_state = PHONE_ACTIVE_CALL;
+        }
+        else if (pin_gu == GPIO_PIN_RESET)
+        {
             phone_state = PHONE_IDLE;
         }
         break;
@@ -153,7 +187,7 @@ void phone_fsm_process(void)
     }
 
     if ((phone_state == PHONE_IDLE) && bt_hfp_uart_done() &&
-        uart_debug_tx_done() && (bt_hfp_get_audio() == HFPAUDIO_DISCONNECTED))
+        uart_debug_tx_done() && (audio_state == HFPAUDIO_DISCONNECTED))
     {
         puts("s");
         while (!bt_hfp_uart_done() || !uart_debug_tx_done())
